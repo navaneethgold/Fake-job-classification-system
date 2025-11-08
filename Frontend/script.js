c20b7bc413ee9cba84befc639605845c8c893b27
navaneethgold <navaneethabs.2006@gmail.com>
2025-11-08 16:03:35 +0530

    built ml model, landing page

--- Frontend/script.js
+++ Frontend/script.js
@@ -17,7 +17,7 @@ form.addEventListener("submit", async (event) => {
   };
 
   try {
-    const response = await fetch("http://127.0.0.1:8000/predict", {
+    const response = await fetch("/predict", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
